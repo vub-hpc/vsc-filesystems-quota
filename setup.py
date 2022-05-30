06ea053f187fcf5ca74a1af41e54eb2e750f8907
Alex Domingo <alex.domingo.toro@vub.be>
2022-05-30 16:42:25 +0200

    update requirements

--- setup.py
+++ setup.py
@@ -32,8 +32,8 @@ PACKAGE = {
     'install_requires': [
         'vsc-accountpage-clients >= 2.0.0',
         'vsc-base >= 3.0.6',
-        'vsc-config >= 3.0.1',
-        'vsc-filesystems >= 1.2.17',
+        'vsc-config >= 3.11.0',
+        'vsc-filesystems >= 1.3.0',
         'vsc-utils >= 2.0.0',
     ],
     'tests_require': ['mock'],
